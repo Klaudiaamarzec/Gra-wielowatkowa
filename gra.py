bc3776d7e277d2cd99a11cc8b2a8741531f3a1db
Klaudiaamarzec <klaudia06072002@onet.pl>
2024-03-19 15:36:47 +0100

    Działające poruszanie się

--- gra.py
+++ gra.py
@@ -1,4 +1,5 @@
 import pygame
+import threading
 import time
 import random
 
@@ -19,13 +20,38 @@ class Player:
         elif pressed_keys[self.keys['right']]:
             self.rect.move_ip(5, 0)
 
+class CashThread(threading.Thread):
+    def __init__(self, player1, player2, screen):
+        super(CashThread, self).__init__()
+        self.player1 = player1
+        self.player2 = player2
+        self.screen = screen
+        self.cash = []
+        self.running = True
+
+    def generate_cash(self):
+        # Cash position draw
+        x = random.randint(0, 750)
+        y = random.randint(0, 550)
+        self.cash.append((x, y))
+
+    def run(self):
+        while self.running:
+
+            # Cash generate
+            self.generate_cash()
+            time.sleep(5)  # Example delay for generated cash
+
+    def stop(self):
+        self.running = False
+
 def main():
     pygame.init()
 
     # Window settings
     width, height = 800, 600
     screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("Fireboy and Watergirl")
+    pygame.display.set_caption("Fast and Furious")
 
     # Colors
     white = (255, 255, 255)
@@ -33,8 +59,12 @@ def main():
     blue = (0, 0, 255)
 
     # Players
-    player1 = Player("Player 1", red, 50, 50, {'up': pygame.K_UP, 'down': pygame.K_DOWN, 'left': pygame.K_LEFT, 'right': pygame.K_RIGHT})
-    player2 = Player("Player 2", blue, 700, 500, {'up': pygame.K_w, 'down': pygame.K_s, 'left': pygame.K_a, 'right': pygame.K_d})
+    player1 = Player("Player 1", red, 150, 500, {'up': pygame.K_UP, 'down': pygame.K_DOWN, 'left': pygame.K_LEFT, 'right': pygame.K_RIGHT})
+    player2 = Player("Player 2", blue, 600, 500, {'up': pygame.K_w, 'down': pygame.K_s, 'left': pygame.K_a, 'right': pygame.K_d})
+
+    # Thread for cash
+    cash_thread = CashThread(player1, player2, screen)
+    cash_thread.start()
 
     running = True
     clock = pygame.time.Clock()
@@ -51,9 +81,17 @@ def main():
         screen.fill(white)
         pygame.draw.rect(screen, player1.color, player1.rect)
         pygame.draw.rect(screen, player2.color, player2.rect)
+
+        # drawing cash on the screen
+        for cash in cash_thread.cash:
+            pygame.draw.rect(screen, (255, 255, 0), pygame.Rect(cash[0], cash[1], 10, 10))
+
         pygame.display.flip()
         clock.tick(60)
 
+        # Stop cash thread
+    cash_thread.stop()
+    cash_thread.join()
     pygame.quit()
 
 if __name__ == "__main__":
